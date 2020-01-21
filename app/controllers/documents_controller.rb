1b4d897b61273a3bab61af287917740056cca6b2
github username <github email address>
2020-01-21 09:29:17 +0000

    Initial Commit

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,28 @@
+class DocumentsController < ApplicationController
+        def index
+        @documents = Document.all
+    end
+
+    def show
+        @document = Document.find(params[:id])
+    end
+
+    def new
+        @document = Document.new
+    end
+
+    def create
+        @document = Document.create(data: params[:data])
+        redirect_to documents_path
+    end
+
+    def edit
+        @document = Document.find(params[:id])
+    end
+
+        def update
+        @document = Document.find(params[:id])
+        @document.update(data: params[:document][:data])
+        redirect_to documents_path
+    end
+end
